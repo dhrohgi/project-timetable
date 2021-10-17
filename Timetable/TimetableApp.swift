993c800e00499948aa7d3d41d4494d03ca186149
Daehan Roh <dhrohgo@gmail.com>
2021-10-17 10:47:59 -0500

    Initial Commit

---
+++ Timetable/TimetableApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimetableApp.swift
+//  Timetable
+//
+//  Created by dennis on 10/17/21.
+//
+
+import SwiftUI
+
+@main
+struct TimetableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
